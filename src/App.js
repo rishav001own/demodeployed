c1a85b688c6ede9fdbe9ecb3115caa0302e3021e
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-08-10 14:03:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+import Login from './components/login';
+import Signup from './components/signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <Switch>
+      <Route exact path="/" component={Login}></Route>
+        <Route path="/signup" component={Signup}></Route>
+        <Route path="/home">
+          <h1>Welcome to Homepage And sucessfully loggedIn</h1>
+        </Route>
+      </Switch>
+      </Router>
   );
 }
 
